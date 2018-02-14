ea14207c2d592c9fd65a21190147e528b605c341
elvis <rojohe@163.com>
2018-02-15 03:53:58 +0800

    2chagne admin.java

--- Admin.java
+++ Admin.java
@@ -2,4 +2,6 @@ import java.io.Serializable;
 public class Admin implements Serializable{
 	private String adminId;
     private String password;
+    private Integer flag;
+    private String name;
 }
