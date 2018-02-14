f6a8cc8e683d77d07ca4304d626094d1aee0b022
elvis <rojohe@163.com>
2018-02-14 14:24:41 +0800

    add static method

--- Hello.java
+++ Hello.java
@@ -4,7 +4,12 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Happy new year");
 		System.out.println("Happy myself");
-
+		print();
+    }
+	public static void print(){
+		System.out.println("*******start*****");
+		System.out.println("hello world!");
+		System.out.println("*******end*****");
 	}
 
 }
