97c0438d7dfa71ead81b0cbc1806a1e52aff00b0
elvis <rojohe@163.com>
2018-02-14 14:31:53 +0800

    simple print

--- Hello.java
+++ Hello.java
@@ -1,15 +1,8 @@
 
 public class Hello {
-    public final static String COMPANY_NAME = "yootk";
-	public static void main(String[] args) {
-		System.out.println("Happy new year");
-		System.out.println("Happy myself");
-		print();
-    }
-	public static void print(){
-		System.out.println("*******start*****");
-		System.out.println("hello world!");
-		System.out.println("*******end*****");
-	}
 
+	public static void main(String[] args) {
+        System.out.println("***********");	
+        System.out.println("###########");	
+    }
 }
