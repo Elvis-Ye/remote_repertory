5c6e44adf809d15ce0a35e9e8c6f213d256cb262
elvis <rojohe@163.com>
2018-02-14 14:59:51 +0800

    conflict print

--- Hello.java
+++ Hello.java
@@ -3,12 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
         System.out.println("Hello world");	
-<<<<<<< HEAD
         System.out.println("Hello ye");	
         System.out.println("**");
-=======
-        System.out.println("Hello ye");
         System.out.println("#");
->>>>>>> brh
     }
 }
