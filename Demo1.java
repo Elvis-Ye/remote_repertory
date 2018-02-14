e2a5e0c9a0d9afb7564d7c347f26370e16d38927
elvis <rojohe@163.com>
2018-02-14 11:17:56 +0800

    master file

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+
+
+public class Demo1 {
+	public static void main(String[] args) {
+       System.out.println("new date"+new Date());
+       System.out.println("remote repository");
+	}
+}
