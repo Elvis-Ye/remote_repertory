479f5da0b5fedcf77b211d66ae4a4d64299c8ea1
elvis <rojohe@163.com>
2018-02-15 03:49:39 +0800

    chagne admin.java

--- Admin.java
+++ Admin.java
@@ -1,5 +1,7 @@
 import java.io.Serializable;
+import java.util.Date;
 public class Admin implements Serializable{
 	private String adminId;
     private String password;
+    private Date lastDate;
 }
